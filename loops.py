9e3f3c9e27e6c6bffae8181ac5df9b0696948950
HardikBhortakke <96573636+HardikBhortakke@users.noreply.github.com>
2023-04-28 15:05:15 +0530

    Create loops.py
    Added loops.py

---
+++ loops.py
@@ -0,0 +1,38 @@
+# loops in python 
+
+# while loop 
+# syntax - 
+# while (condition):
+#     statements 
+
+i = 1
+while (i <= 10):
+    print(i*5,'\n')
+    i += 1
+
+# while - else loop 
+# syntax - 
+# while (condition):
+#     statements 
+# else:
+#     statements - executed once
+
+# c = 1
+# while (c <= 10):
+#     print(c*c,"\n")
+# else:
+#     print("End of loop")
+
+# for loop 
+# syntax -
+# for <variable> in <range/bound>:
+#     statements
+
+# for i in range(1,11):
+#     print(i*i,"\n")
+
+# for i in range(11):
+#     print(i*i,"\n")
+
+# for i in range(0,20,2):
+#     print(i,"\n")
