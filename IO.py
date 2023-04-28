8b51eef2688b38d67ca6b1e88ce1d5e0c572fe1f
HardikBhortakke <96573636+HardikBhortakke@users.noreply.github.com>
2023-04-28 15:02:24 +0530

    Create IO.py
    Added IO.py

---
+++ IO.py
@@ -0,0 +1,13 @@
+a = input("Enter a number:")
+print("Your number ",a," is of type",type(a))
+# a2 = int(input("Enter an integral value:"))
+# print("Your number is",a2," and is of type",type(a2))
+
+# b,c,d = [int(x) for x in input("Enter Three numbers:").split()] 
+# print(a,"\n",b,"\n",c)
+
+# e = float(input("Enter a float value"))
+# print("The floating value of e is ",e)
+
+# f = str(input("Enter the string "))
+# print("The string is :",f)
