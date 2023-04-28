e92afdd028d96a8cb98a5a9a0fbbef51d5460f09
HardikBhortakke <96573636+HardikBhortakke@users.noreply.github.com>
2023-04-28 15:04:08 +0530

    Create control_statements.py
    Added control_statements.py

---
+++ control_statements.py
@@ -0,0 +1,20 @@
+# control statements - continue,break,pass
+
+for i in range (11):
+    if(i % 2 == 0):
+        continue
+        print(i)
+    else :
+        print(i)
+
+# for i in range (11):
+#     if(i == 5):
+#         break
+#     else :
+#         print(i)
+
+# for i in range (11):
+#     if(i % 2 == 0):
+#         pass
+#     else :
+#         print(i)
