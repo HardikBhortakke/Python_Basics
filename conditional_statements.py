1148ace5a5f2396dd6d965fab8285b45fc55cd7c
HardikBhortakke <96573636+HardikBhortakke@users.noreply.github.com>
2023-04-28 15:06:21 +0530

    Add files via upload
    added conditional_statements.py

---
+++ conditional_statements.py
@@ -0,0 +1,51 @@
+#conditional statements
+# if else conditional statements
+# syntax -
+#if (condition):
+#    statements
+# else:
+#     statements 
+ 
+# x = int(input("Enter a number\n"))
+# if (x == 5):
+#     print("True")
+# else:
+#     print("False")
+
+# elif statements
+# syntax -
+# if(condition):
+#     statements
+# elif(condition):
+#     statements 
+# else:
+#     statements
+
+# day = int(input("Enter Day - "))
+# if (day == 1):
+#     print("MARIO day 1 - Friday")
+# elif (day == 2):
+#     print("MARIO day 2 - Saturday")
+# elif (day == 3):
+#     print("MARIO day 3 - Sunday")
+# else:
+#     print("No MARIO workshop Today")
+
+# nested if-else condition 
+# syntax -
+# if (condition):
+#     if (condition):
+#         ....
+#         statemens
+#     else:
+#         statements
+#     ...
+# else :
+#     statements
+
+# i = int(input("Enter a number\n"))
+# if (i <= 10):
+#     if (i % 2 == 0):
+#         print(i," is even")
+# else:
+#     print("out of bound")
